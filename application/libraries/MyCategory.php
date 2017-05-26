4ee9fd8b866acedae81019b8c3ab3aeb414b0004
KhoaZero123 <khoazero123@gmail.com>
2017-05-26 16:31:24 +0700

    Fix fontawesome

--- application/libraries/MyCategory.php
+++ application/libraries/MyCategory.php
@@ -45,7 +45,28 @@ class MyCategory {
         }
         return $html;
     }
-	function generateNavHTMLCategories($parent, $category,$level=0,$current=null) {
+    function generateNavHTMLCategories($parent, $category,$level=0,$current=null) {
+        
+        $html = '';
+        if (isset($category['parent_cats'][$parent])) {
+            foreach ($category['parent_cats'][$parent] as $cat_id) {
+                $html .= '<li class="level'.$level.''.($level==0 ? ' level-top':'').' nav-'.$this->nav.' '.(isset($category['parent_cats'][$cat_id]) ? ' parent':'').''.($category['categories'][$cat_id]['id']==$current ? ' active':'').'">
+                            <a'.($level==0 ? ' class="level-top"':'').' href="category/'.$category['categories'][$cat_id]['slug'].'.html">
+                                <span>'.$category['categories'][$cat_id]['name'].'</span>
+                                '.($level==0 ? '<span class="boder-menu"></span>':'').'
+                            </a>';
+                if (isset($category['parent_cats'][$cat_id])) {
+                    $html .= '<ul class="level'.$level.'">'."\n";
+                    $html .= $this->generateNavHTMLCategories($cat_id, $category,$level+1);
+                    $html .= '</ul>'."\n";
+                }
+                $html .= '</li>'."\n";
+                if($level==0) $this->nav++;
+            }
+        }
+        return $html;
+    }
+	function generateBoxMenuHTMLCategories($parent, $category,$level=0,$current=null) {
 		
         $html = '';
         if (isset($category['parent_cats'][$parent])) {
